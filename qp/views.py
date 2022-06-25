856b59cc2097d9d4346220877307f0f966dafb1e
M-C Pronovost <pronovost.mc@outlook.com>
2022-06-24 21:53:47 -0400

    update app view

--- qp/views.py
+++ qp/views.py
@@ -9,17 +9,20 @@ def app(request):
     js = []
     stage = "dev" if DEBUG else "pro"
     # ===---
-    with open("frontend/webpack-%s.json" % (stage), "r") as f:
-        wp = json.load(f)
-        for chunk in wp["chunks"]["app"]:
-            if chunk.startswith("css"):
-                css.append(
-                    static("/vue/%s/%s" % (stage, chunk))
-                )
-            elif chunk.startswith("js"):
-                js.append(
-                    static("/vue/%s/%s" % (stage, chunk))
-                )
+    try:
+        with open("frontend/webpack-%s.json" % (stage), "r") as f:
+            wp = json.load(f)
+            for chunk in wp["chunks"]["app"]:
+                if chunk.startswith("css"):
+                    css.append(
+                        static("/vue/%s/%s" % (stage, chunk))
+                    )
+                elif chunk.startswith("js"):
+                    js.append(
+                        static("/vue/%s/%s" % (stage, chunk))
+                    )
+    except Exception as e:
+        print(e)
     # ===---
     context = {
         "css": css,
