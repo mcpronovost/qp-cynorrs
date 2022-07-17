c38887d2a78615582da94fe028f98ad3816610bb
M-C Pronovost <pronovost.mc@outlook.com>
2022-07-17 16:22:28 -0400

    update qpCard style

--- frontend/src/components/basic/qpCard.vue
+++ frontend/src/components/basic/qpCard.vue
@@ -61,20 +61,19 @@ const marginBottom = computed(() => {
 <style scoped>
     .qp-card {
         background-color: var(--qp-base);
-        color: var(--qp-primary);
+        color: var(--qp-secondary);
         border-radius: 4px;
-        box-shadow: 0 0 3px rgba(67, 82, 89, 0.04);
+        box-shadow: 0 0 3px rgba(0, 0, 0, 0.04);
         overflow: hidden;
         text-align: center;
         align-content: center;
         justify-content: center;
         height: 100%;
-        transition: box-shadow 0.3s, opacity 0.3s;
+        transition: box-shadow 0.3s, color 0.3s, opacity 0.3s;
     }
     .qp-card.qp-card-clickable:hover {
-        box-shadow: 0 0 6px rgba(67, 82, 89, 0.6);
+        color: var(--qp-primary);
         cursor: pointer;
-        opacity: 0.8;
     }
     .qp-card.qp-card-overflow {
         overflow: visible;
